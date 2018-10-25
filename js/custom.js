c4f5dffef33cec0f85d6f7eeb81e3aa572c457f5
nitesh624 <nsinha624@gmail.com>
2018-10-25 12:23:44 +0530

    custom-style.css updated

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,64 @@
-/* --------------------------------------------- 
 
-* Filename:     custom-style.js
-* Version:      1.0.0 (2016-01-12)
-* Website:      http://www.zymphonies.com
-                http://www.freebiezz.com
-* Description:  System JS
-* Author:       Zymphonies Dev Team
-                info@zymphonies.com
+!function($) {
 
------------------------------------------------*/
+ var mainHeader = $('#main-header'),
+  		secondaryNavigation = $('.cd-secondary-nav'),
+  		//this applies only if secondary nav is below intro section
+  		belowNavHeroContent = $('.sub-nav-hero'),
+  		headerHeight = mainHeader.height();
+
+  	//set scrolling variables
+  	var scrolling = false,
+  		previousTop = 0,
+  		currentTop = 0,
+  		scrollDelta = 2,
+  		scrollOffset = 100;
+
+  	mainHeader.on('click', '.nav-trigger', function(event){
+  		// open primary navigation on mobile
+  		event.preventDefault();
+  		mainHeader.toggleClass('nav-open');
+  	});
+
+  	$(window).on('scroll', function(){
+  		if( !scrolling ) {
+  			scrolling = true;
+  			(!window.requestAnimationFrame)
+  				? setTimeout(autoHideHeader, 250)
+  				: requestAnimationFrame(autoHideHeader);
+  		}
+  	});
+
+  	$(window).on('resize', function(){
+  		headerHeight = mainHeader.height();
+  	});
+
+  	function autoHideHeader() {
+  		var currentTop = $(window).scrollTop();
+
+  		( belowNavHeroContent.length > 0 )
+  			? checkStickyNavigation(currentTop) // secondary navigation below intro
+  			: checkSimpleNavigation(currentTop);
+
+  	   	previousTop = currentTop;
+  		scrolling = false;
+  	}
+
+  	function checkSimpleNavigation(currentTop) {
+		  //there's no secondary nav or secondary nav is below primary nav
+		  if ( $(window).width() > 767 ){
+			if (previousTop - currentTop > scrollDelta) {
+				//if scrolling up...
+				mainHeader.removeClass('is-hidden');
+			}
+			else if( currentTop - previousTop > scrollDelta && currentTop > scrollOffset) {
+				//if scrolling down...
+				mainHeader.addClass('is-hidden');
+			}
+		}
+	  }
+
+
 
 jQuery(document).ready(function($){
 
@@ -27,3 +77,6 @@ jQuery(document).ready(function($){
 	    });
 	}
 });
+
+
+}(jQuery);
